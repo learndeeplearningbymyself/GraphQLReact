6364b6a779808f743c040157fda889c1783d3059
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2019-07-21 18:16:39 +0900

    Complete until section 17

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,15 +1,47 @@
 const graphql = require('graphql');
+const axios = require('axios');
 const {
+    GraphQLSchema,
     GraphQLObjectType,
     GraphQLInt,
     GraphQLString
 } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+});
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+        company: {
+            type: CompanyType
+        }
     }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios.default.get(`http://localhost:3000/users/${args.id}`)
+                    .then(resp => resp.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
