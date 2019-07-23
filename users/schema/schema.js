8873ee1a8f12485f69d1ade2a496c9c093bc7b09
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2019-07-23 22:42:39 +0900

    complate until section 26

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -5,7 +5,8 @@ const {
     GraphQLObjectType,
     GraphQLInt,
     GraphQLString,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -68,17 +69,19 @@ const mutation = new GraphQLObjectType({
         addUser: {
             type: UserType,
             args: {
-                firstName: { type: GraphQLString },
-                age: { type: GraphQLInt },
+                firstName: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
                 companyId: { type: GraphQLString }
             },
-            resolve() {
-
+            resolve(parentValue, { firstName, age }) {
+                return axios.post('http://localhost:3000/users', { firstName, age })
+                    .then(res => res.data);
             }
         }
     }
 })
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 });
